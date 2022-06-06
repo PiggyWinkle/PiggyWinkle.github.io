752b82d341bb42890125c213f35aeac5c52d56cb
PiggyWinkle <96551672+PiggyWinkle@users.noreply.github.com>
2022-06-05 20:09:02 -0400

    modal update

--- card.js
+++ card.js
@@ -133,28 +133,26 @@ function SubForm(){
 });
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+//Modal modal modal
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function modal(){
+let modalBtn = document.getElementById("modal-btn")
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let modal = document.querySelector(".modal")
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+let closeBtn = document.querySelector(".close-btn")
+
+modalBtn.onclick = function(){
+  modal.style.display = "block"
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+closeBtn.onclick = function(){
+  modal.style.display = "none"
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
   }
-} 
+}
+}
