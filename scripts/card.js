76859ebecc88c4a10ec74f7d4b6559de79d8d429
PiggyWinkle <96551672+PiggyWinkle@users.noreply.github.com>
2022-06-19 20:34:44 -0400

    confetti timing

--- scripts/card.js
+++ scripts/card.js
@@ -218,7 +218,7 @@ function checkWin(){
     const start = () => {
       setTimeout(function() {
           confetti.start()
-      }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+      }, 250); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
   };
 
   //  for stopping the confetti 
